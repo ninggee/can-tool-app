a02f67ba2e216c81a753d913d06ae380ea59fdfd
linhuarui1995 <764058804@qq.com>
2017-10-10 17:54:27 +0800

    changelocation

---
+++ File/林华睿界面/MyBottom/app/src/main/java/com/example/lhr/mybottom/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.lhr.mybottom;
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
