b1db4c1872d68edca656c7b6c2d3c1c69924af73
linhuarui1995 <764058804@qq.com>
2017-10-10 18:39:36 +0800

    shift

--- File/林华睿界面/MyBottom/app/src/main/java/com/example/lhr/mybottom/MainActivity.java
+++ File/林华睿界面/MyBottom/app/src/main/java/com/example/lhr/mybottom/MainActivity.java
@@ -2,16 +2,73 @@ package com.example.lhr.mybottom;
 
 import android.app.ActionBar;
 import android.app.Activity;
+import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends Activity {
+import com.roughike.bottombar.BottomBar;
+import com.roughike.bottombar.BottomBarTab;
+import com.roughike.bottombar.OnTabReselectListener;
+import com.roughike.bottombar.OnTabSelectListener;
+
+public class MainActivity extends Activity implements View.OnClickListener{
+
+    private BottomBar bottomBar;
+
+    private BottomBarTab nearby;
+
+    private TextView messageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bottomBar = (BottomBar) findViewById(R.id.bottomBar);
+
+        messageView = (TextView) findViewById(R.id.messageView);
+
+        BottomBar bottomBar = (BottomBar) findViewById(R.id.bottomBar);
+        bottomBar.setOnTabSelectListener(new OnTabSelectListener() {
+            @Override
+            public void onTabSelected(@IdRes int tabId) {
+                messageView.setText(TabMessage.get(tabId, false));
+            }
+        });
+
+        bottomBar.setOnTabReselectListener(new OnTabReselectListener() {
+            @Override
+            public void onTabReSelected(@IdRes int tabId) {
+                Toast.makeText(getApplicationContext(), TabMessage.get(tabId, true), Toast.LENGTH_LONG).show();
+            }
+        });
+/*        bottomBar.setOnTabSelectListener(new OnTabSelectListener() {
+            public void onTabSelected(@IdRes int tabId) {
+                if (tabId == R.id.tab_favorites) {
+                    // 选择指定 id 的标签
+                    nearby = bottomBar.getTabWithId(R.id.tab_nearby);
+                    nearby.setBadgeCount(5);
+                }
+            }
+        });
+
+        bottomBar.setOnTabReselectListener(new OnTabReselectListener() {
+            public void onTabReSelected(@IdRes int tabId) {
+                if (tabId == R.id.tab_favorites) {
+                    // 已经选择了这个标签，又点击一次。即重选。
+                    nearby.removeBadge();
+                }
+            }
+        });*/
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
---
+++ File/林华睿界面/MyBottom/app/src/main/java/com/example/lhr/mybottom/TabMessage.java
@@ -0,0 +1,34 @@
+package com.example.lhr.mybottom;
+
+/**
+ * Created by iiro on 7.6.2016.
+ */
+public class TabMessage {
+    public static String get(int menuItemId, boolean isReselection) {
+        String message = "Content for ";
+
+        switch (menuItemId) {
+            case R.id.tab_recents:
+                message += "recents";
+                break;
+            case R.id.tab_favorites:
+                message += "favorites";
+                break;
+            case R.id.tab_nearby:
+                message += "nearby";
+                break;
+           /* case R.id.ta:
+                message += "friends";
+                break;
+            case R.id.tab_food:
+                message += "food";
+                break;*/
+        }
+
+        if (isReselection) {
+            message += " WAS RESELECTED! YAY!";
+        }
+
+        return message;
+    }
+}
