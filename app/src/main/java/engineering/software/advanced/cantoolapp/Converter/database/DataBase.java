5001cc52ab0b90a9fb0a18873f1cd22969e0ce20
linhuarui1995 <764058804@qq.com>
2017-10-11 22:37:25 +0800

    searchforsignal

---
+++ app/src/main/java/engineering/software/advanced/cantoolapp/Converter/database/DataBase.java
@@ -0,0 +1,16 @@
+package engineering.software.advanced.cantoolapp.Converter.database;
+
+import engineering.software.advanced.cantoolapp.Converter.Entity.CanMessage;
+import engineering.software.advanced.cantoolapp.Converter.Entity.CanSignal;
+
+/**
+ * Created by lhr on 2017/10/11.
+ */
+
+public interface DataBase {
+    //根据ID寻找message
+    public CanMessage searchMessageUseId(Long id);
+
+    //根据message寻找后面的signal
+    public CanSignal searchSignalUseMessage(CanMessage message);
+}
