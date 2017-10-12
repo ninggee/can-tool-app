b394a6a97cd3d6145d77a564f457b79022905803
ningge <weiguangningtian@gmail.com>
2017-10-12 13:49:12 +0800

    little modification

--- app/src/main/java/engineering/software/advanced/cantoolapp/communicator/Reader.java
+++ app/src/main/java/engineering/software/advanced/cantoolapp/communicator/Reader.java
@@ -4,7 +4,7 @@ package engineering.software.advanced.cantoolapp.communicator;
  * Created by ningge on 11/10/2017.
  */
 
-public abstract interface Reader {
+public interface Reader {
 
-    public abstract void read();
+   void read();
 }
