4e99d1b2b412d20f35ba2a4f0053bfc39c6c5a0c
Dandy-John <zdd12@qq.com>
2017-10-12 23:19:44 +0800

    handle conflicts

--- app/src/test/java/engineering/software/advanced/cantoolapp/test/Converter/TestDatabase.java
+++ app/src/test/java/engineering/software/advanced/cantoolapp/test/Converter/TestDatabase.java
@@ -15,17 +15,10 @@ import engineering.software.advanced.cantoolapp.Converter.database.Impl.DataBase
 public class TestDatabase {
     @Test
     public void testDatabase(){
-<<<<<<< HEAD
         DataBaseImpl db = new DataBaseImpl();
-        System.out.println(db.searchMessageUseId((long)1056));
-        System.out.println("----------------------");
-        Set<CanSignal> list = db.searchSignalUseMessage(db.searchMessageUseId((long)1056));
-=======
-        DatabaseImpl db = new DatabaseImpl();
         System.out.println(db.searchMessageUseId((long)856));
         System.out.println("----------------------");
-        List<CanSignal> list = db.searchSignalUseMessage(db.searchMessageUseId((long)856));
->>>>>>> d38393274c15b92b883b4b542764e92695cc354f
+        Set<CanSignal> list = db.searchSignalUseMessage(db.searchMessageUseId((long)856));
         System.out.println(list.size());
         for(CanSignal cs : list)
             System.out.println(cs);
