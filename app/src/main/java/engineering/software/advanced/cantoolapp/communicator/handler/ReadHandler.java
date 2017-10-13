4a66ad831de960951f31ec166ef69a73df7401a5
ningge <weiguangningtian@gmail.com>
2017-10-13 20:16:39 +0800

    add readHandler

---
+++ app/src/main/java/engineering/software/advanced/cantoolapp/communicator/handler/ReadHandler.java
@@ -0,0 +1,20 @@
+package engineering.software.advanced.cantoolapp.communicator.handler;
+
+import android.os.Message;
+
+/**
+ * Created by ningge on 2017/10/13.
+ */
+
+public class ReadHandler extends Handler {
+    private static String TAG = "ReadHandler";
+    @Override
+    public void handleMessage(Message msg) {
+        super.handleMessage(msg);
+    }
+
+    @Override
+    public void handle(String message) {
+
+    }
+}
