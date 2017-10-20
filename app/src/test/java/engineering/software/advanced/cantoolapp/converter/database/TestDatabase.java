307415df469bf7429d7c911e3725d942c12faca3
ningge <weiguangningtian@gmail.com>
2017-10-20 20:20:12 +0800

    hide wrong test code

--- app/src/test/java/engineering/software/advanced/cantoolapp/converter/database/TestDatabase.java
+++ app/src/test/java/engineering/software/advanced/cantoolapp/converter/database/TestDatabase.java
@@ -2,10 +2,7 @@ package engineering.software.advanced.cantoolapp.converter.database;
 
 import org.junit.Test;
 
-import java.util.Set;
-
 import engineering.software.advanced.cantoolapp.converter.database.Impl.DataBaseImpl;
-import engineering.software.advanced.cantoolapp.converter.entity.CanSignal;
 
 /**
  * Created by lhr on 2017/10/11.
@@ -15,11 +12,11 @@ public class TestDatabase {
     @Test
     public void testDatabase() throws Exception {
         DataBase db = DataBaseImpl.getInstance();
-        System.out.println(db.searchMessageUseId((long)856, isr));
-        System.out.println("----------------------");
-        Set<CanSignal> list = db.searchSignalUseMessage(db.searchMessageUseId((long)856, isr),isr);
-        System.out.println(list.size());
-        for(CanSignal cs : list)
-            System.out.println(cs);
+//        System.out.println(db.searchMessageUseId((long)856, isr));
+//        System.out.println("----------------------");
+//        Set<CanSignal> list = db.searchSignalUseMessage(db.searchMessageUseId((long)856, isr),isr);
+//        System.out.println(list.size());
+//        for(CanSignal cs : list)
+//            System.out.println(cs);
     }
 }
