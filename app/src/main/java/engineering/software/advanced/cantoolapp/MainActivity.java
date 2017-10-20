f27bb982cfb3e6645d95e0d97e593abdd6df64ba
ningge <weiguangningtian@gmail.com>
2017-10-20 19:51:29 +0800

    move drawer compontent initialize to other function

--- app/src/main/java/engineering/software/advanced/cantoolapp/MainActivity.java
+++ app/src/main/java/engineering/software/advanced/cantoolapp/MainActivity.java
@@ -3,20 +3,19 @@ package engineering.software.advanced.cantoolapp;
 import android.content.res.AssetManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.util.Log;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -34,52 +33,11 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        //init drawer compontent
+        __initDrawer();
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
-        toggle.syncState();
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
-
-        View.OnClickListener onClickListener=new View.OnClickListener() {
-            @Override
-            public void onClick(View v){
-                Toast.makeText(MainActivity.this,"Button点击事件1", Toast.LENGTH_LONG).show();
-                InputStreamReader isr = null;
-                try {
-                    AssetManager am = getResources().getAssets();
-                    //打开assets中文件
-                    InputStream is = am.open("canmsg-sample.dbc");
-                    isr =new InputStreamReader(is,"GBK");
-                    DataBase db = DataBaseImpl.getInstance();
-                    CanMessage cm = db.searchMessageUseId((long)856, isr);
-                    Log.i("tag", String.valueOf(cm.getId()));
-                    is = am.open("canmsg-sample.dbc");
-                    isr =new InputStreamReader(is,"GBK");
-                    Set<CanSignal> list = db.searchSignalUseMessage(cm,isr);
-                    for(CanSignal cs : list)
-                        Log.i("tag", cs.toString());
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-
-            }
-        };
-        findViewById(R.id.b1).setOnClickListener(onClickListener);
 
     }
 
@@ -138,5 +96,55 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+
+    private void __initDrawer() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        View.OnClickListener onClickListener=new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                Toast.makeText(MainActivity.this,"Button点击事件1", Toast.LENGTH_LONG).show();
+                InputStreamReader isr = null;
+                try {
+                    AssetManager am = getResources().getAssets();
+                    //打开assets中文件
+                    InputStream is = am.open("canmsg-sample.dbc");
+                    isr =new InputStreamReader(is,"GBK");
+                    DataBase db = DataBaseImpl.getInstance();
+                    CanMessage cm = db.searchMessageUseId((long)856, isr);
+                    Log.i("tag", String.valueOf(cm.getId()));
+                    is = am.open("canmsg-sample.dbc");
+                    isr =new InputStreamReader(is,"GBK");
+                    Set<CanSignal> list = db.searchSignalUseMessage(cm,isr);
+                    for(CanSignal cs : list)
+                        Log.i("tag", cs.toString());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        };
+        findViewById(R.id.b1).setOnClickListener(onClickListener);
     }
 }
---
+++ app/src/main/java/engineering/software/advanced/cantoolapp/web_app_interfaces/TestInterface.java
@@ -0,0 +1,8 @@
+package engineering.software.advanced.cantoolapp.web_app_interfaces;
+
+/**
+ * Created by ningge on 20/10/2017.
+ */
+
+public class TestInterface {
+}
