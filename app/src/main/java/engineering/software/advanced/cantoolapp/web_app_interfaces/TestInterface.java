1066d08d66786e794544640807dca4d5cbb967e4
ningge <weiguangningtian@gmail.com>
2017-10-20 19:51:59 +0800

    test javascript interface

--- app/src/main/java/engineering/software/advanced/cantoolapp/web_app_interfaces/TestInterface.java
+++ app/src/main/java/engineering/software/advanced/cantoolapp/web_app_interfaces/TestInterface.java
@@ -1,8 +1,23 @@
 package engineering.software.advanced.cantoolapp.web_app_interfaces;
 
+import android.content.Context;
+import android.webkit.JavascriptInterface;
+import android.widget.Toast;
+
 /**
  * Created by ningge on 20/10/2017.
  */
 
 public class TestInterface {
+
+    Context __context;
+
+    TestInterface(Context c) {
+        __context = c;
+    }
+
+    @JavascriptInterface
+    public void showToast(String toast) {
+        Toast.makeText(__context, toast, Toast.LENGTH_LONG).show();
+    }
 }
