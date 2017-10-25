2110c16b088efb07153090bcf30aecba23184a99
Dandy-John <zdd12@qq.com>
2017-10-25 21:58:25 +0800

    Revert "complete toxml"
    This reverts commit f1bff44e6bd75157903e2a749bcb78d6a1ff5eb2.

--- app/src/main/java/engineering/software/advanced/cantoolapp/converter/entity/Signal.java
+++ app/src/main/java/engineering/software/advanced/cantoolapp/converter/entity/Signal.java
@@ -1,21 +1,14 @@
 package engineering.software.advanced.cantoolapp.converter.entity;
 
-import javax.xml.bind.annotation.XmlRootElement;
-
 /**
  * Created by Zhang Dongdi on 2017/10/12.
  */
 //解析后的信号
-@XmlRootElement
 public class Signal {
     private String name;
     private String origin;
     private double value;
     private CanSignal canSignal;
-
-    public Signal() {
-
-    }
 
     public Signal(String name, String origin, double value, CanSignal canSignal) {
         this.name = name;
@@ -28,31 +21,15 @@ public class Signal {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getOrigin() {
         return origin;
-    }
-
-    public void setOrigin(String origin) {
-        this.origin = origin;
     }
 
     public double getValue() {
         return value;
     }
 
-    public void setValue(double value) {
-        this.value = value;
-    }
-
     public CanSignal getCanSignal() {
         return canSignal;
-    }
-
-    public void setCanSignal(CanSignal canSignal) {
-        this.canSignal = canSignal;
     }
 }
