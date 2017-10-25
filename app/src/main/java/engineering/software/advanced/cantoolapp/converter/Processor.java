92debc05de25d98340fd76e996016de112746527
Dandy-John <zdd12@qq.com>
2017-10-25 20:17:24 +0800

    modify encode interface and impl

--- app/src/main/java/engineering/software/advanced/cantoolapp/converter/Processor.java
+++ app/src/main/java/engineering/software/advanced/cantoolapp/converter/Processor.java
@@ -1,5 +1,6 @@
 package engineering.software.advanced.cantoolapp.converter;
 
+import java.util.Map;
 import java.util.Set;
 
 import engineering.software.advanced.cantoolapp.converter.entity.Message;
@@ -17,5 +18,5 @@ public interface Processor {
     public Set<Message> decodeMultiple(String strs);
 
     //将message和signal编码，之后加上周期，得到can数据用于发送
-    public String encode(long messageId, Set<Signal> signals, int period);
+    public String encode(long messageId, Map<String, Double> signalMap, int period);
 }
