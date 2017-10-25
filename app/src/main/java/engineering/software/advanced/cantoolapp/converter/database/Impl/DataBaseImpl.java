b28696aaf5ff3dbaebe3f1028269e4b8770b760a
ningge <weiguangningtian@gmail.com>
2017-10-25 22:47:48 +0800

    update

--- app/src/main/java/engineering/software/advanced/cantoolapp/converter/database/Impl/DataBaseImpl.java
+++ app/src/main/java/engineering/software/advanced/cantoolapp/converter/database/Impl/DataBaseImpl.java
@@ -39,8 +39,8 @@ public class DataBaseImpl implements DataBase {
 
 
     DataBaseImpl() {
-//        filename = new File("/data/data/engineering.software.advanced.cantoolapp/files/canmsg-sample.dbc");//读取文件
-                filename = new File("D:\\codes\\android\\can-tool-app\\app\\src\\main\\assets\\canmsg-sample.dbc");//读取文件
+        filename = new File("/data/data/engineering.software.advanced.cantoolapp/files/canmsg-sample.dbc");//读取文件
+//                filename = new File("D:\\codes\\android\\can-tool-app\\app\\src\\main\\assets\\canmsg-sample.dbc");//读取文件
     }
 
     DataBaseImpl(String  path) {
